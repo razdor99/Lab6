859ef4d8f9e4853d03a005a3d562fefa02a1869c
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-11-15 21:18:54 +0000

    l

--- led_display.py
+++ led_display.py
@@ -18,6 +18,8 @@ class LEDdisplay():
     self.Shifter.shiftByte(1 << (row-1)) 
     
 class LED8x8():
+  numbers = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+
   def __init__(self, data, latch, clock):
     self.Shifter = Shifter(data, latch, clock)
   def display(self,row):
