48054996a4e78635f48d2347b15193e16d931ac3
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-11-17 19:51:27 +0000

    d

--- led_display.py
+++ led_display.py
@@ -1,13 +1,14 @@
 # LEDdisplay class
 
-import time
+import multiprocessing, time
 from shifter import Shifter    # extend by composition
+import random
 
 class LEDdisplay():
 
   'Class for controlling a 7-segment LED display'
 
-  numbers = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+  #numbers = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   def __init__(self, data, latch, clock):
     self.Shifter = Shifter(data, latch, clock)
@@ -18,10 +19,62 @@ class LEDdisplay():
     self.Shifter.shiftByte(1 << (row-1)) 
     
 class LED8x8():
-  numbers = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+  pattern = [0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]
 
   def __init__(self, data, latch, clock):
     self.Shifter = Shifter(data, latch, clock)
+    myArray = multiprocessing.Array('i',8) #is it 5 bc of positions or 8 bc of bits
+    row = 0 
+    i = 0
+    
+    while True:
+      x = random.randint(1,5)
+      if x == 1:
+        #walk up
+        if row == 7:
+          pass
+        else:
+          row +=1
+        time.sleep(0.1)
+  
+      if x == 2:
+        #walk down
+        if row == 0:
+          pass
+        else:
+          row -=1
+        time.sleep(0.1)
+
+      if x == 3:
+        #walk left
+        myArray[i] = 0
+        if i == 0:
+          pass
+        else:
+          i -=1
+        myArray[i] = 1
+        time.sleep(0.1)
+       
+      if x == 4:
+        #walk right
+        myArray[i] = 0
+        if i == 7:
+          pass
+        else:
+          i+=1
+        myArray[i] = 1
+        time.sleep(0.1)
+
+      if x == 5:
+        #stay still
+        pass
+        time.sleep(0.1)
+
+      p1 = multiprocessing.Process(target=LED8x8.display, args=(myArray,row))
+      p1.daemon = True
+      p1.start()
+    
+       
   def display(self,row):
-    self.Shifter.shiftByte(LED8x8.numbers[row]) # load the row values
-    self.Shifter.shiftByte(1 << (row)) 
+    self.Shifter.shiftByte(LED8x8.pattern[row]) # load the row values
+    self.Shifter.shiftByte(1 << (row))
--- led_with_shifter.py
+++ led_with_shifter.py
@@ -1,5 +1,6 @@
-import time
-from led_display import LEDdisplay, LED8x8
+
+from led_display import LED8x8
+import multiprocessing, time
 
 # Simple demonstration of the LEDdisplay class.
 # Note that we don't need RPi.GPIO here since all the I/O
@@ -8,14 +9,14 @@ from led_display import LEDdisplay, LED8x8
 # pin-agnostic).
 
 dataPin, latchPin, clockPin = 23, 24, 25
-
+L=LED8x8(dataPin, latchPin, clockPin)
 # Pick a number sequence
-sequence = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+#sequence = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
-theLEDdisplay= LED8x8(dataPin, latchPin, clockPin)
+#theLEDdisplay= LED8x8(dataPin, latchPin, clockPin)
+#myArray = multiprocessing.Array('f',3)
+#p1 = multiprocessing.Process(target=theLEDdisplay.display, args=(myArray))
 
-while True:
-  for n in range(len(sequence)):
-    theLEDdisplay.display(n)
-    time.sleep(0.001)
-  
+
+
+    
