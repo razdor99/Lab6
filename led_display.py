5d4e852c9a4b9dd08582f38836eff3d26c906869
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-11-18 00:54:14 +0000

    d

--- led_display.py
+++ led_display.py
@@ -64,7 +64,7 @@ class LED8x8():
         #stay still
         pass
         
-      myArray[i] =  ~mask & column
+      myArray[i] =  mask & ~column
       self.p1 = multiprocessing.Process(target=self.display, args=(myArray,row))
       self.p1.daemon = True
       self.p1.start()
