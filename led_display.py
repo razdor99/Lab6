6a36e9070a5ae91aa6c59e9823cfc8d986ad31d5
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-11-18 01:03:03 +0000

    d

--- led_display.py
+++ led_display.py
@@ -21,7 +21,7 @@ class LED8x8():
   def __init__(self, data, latch, clock):
     myArray = multiprocessing.Array('i',8)
     #myArray[0], myArray[1],myArray[2],myArray[3],myArray[4],myArray[5],myArray[6],myArray[7],= 0b10000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000
-    mask = 0b00000000
+    mask = 0b11111111
     column = 0b00000001
     self.Shifter = Shifter(data, latch, clock)
     row = 5
@@ -64,7 +64,7 @@ class LED8x8():
         #stay still
         pass
         
-      myArray[i] =  ~column & mask
+      myArray[i] = column & mask
       self.p1 = multiprocessing.Process(target=self.display, args=(myArray,row))
       self.p1.daemon = True
       self.p1.start()
