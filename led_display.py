20d4a782b3d794125c3f4ca49685ffdf6cf532ce
Nicholas Vece <91498866+razdor99@users.noreply.github.com>
2021-11-18 01:21:44 +0000

    d

--- led_display.py
+++ led_display.py
@@ -68,7 +68,7 @@ class LED8x8():
       self.p1 = multiprocessing.Process(target=self.display, args=(myArray,row))
       self.p1.daemon = True
       self.p1.start()
-      print(row,i)
+      print(myArray[:])
       time.sleep(0.1)
     
        
